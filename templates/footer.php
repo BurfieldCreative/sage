59c8cc6368368c7304bab481809b2b446c75578c
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-08 13:28:01 +0000

    Rework header and footer for simpler styles and make use of flex

--- templates/footer.php
+++ templates/footer.php
@@ -1,31 +1,54 @@
 <footer class="footer">
-  <div class="container row island-top">
-    <div class='address col-12 col-6-tablet col-4-desktop footer-col-1'>
-      this will be the address
+    
+    <div class="container row island">
+        <div class='address col-12'>
+            <?php get_template_part( 'templates/vcard' ); ?>
+        </div>
     </div>
-    <div class=" col-12 col-6-tablet col-4-desktop footer-col-2">
-      something to go here
+    
+    
+    <div class="container row island-bottom">    
+        <nav class="nav-footer col-12 clear ">
+            <?php
+            if (has_nav_menu('secondary_navigation')) :
+            
+                $args = array( 
+                    'menu_class' => 'menu footer-menu list-inline',        
+                    'theme_location' => 'secondary_navigation',
+                            
+                    'container' => false,
+                    'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    
+                    'depth' => 1,
+                    'fallback_cb' => false,
+            	);
+            	wp_nav_menu( $args );
+            	
+            endif;
+            ?>
+        </nav>
     </div>
-    <div class=" col-12 col-6-tablet col-4-desktop footer-col-3">
-      something else to go here
+    
+    <div class="sub-footer">
+        <div class="container row island-half">
+            <div class="col-12 col-6-tablet">
+                <span class="site-colophon site-colophon--copyright">
+                    Copyright &copy; <?php echo date("Y") ?> <?php echo esc_html( get_bloginfo( 'name', 'display' ) ); ?>
+                </span>
+            </div>
+            <div class="col-12 col-6-tablet text-right">
+                <span class="site-credit">
+                    <?php 
+                        $site_address   = ( WP_ENV == 'production' ) ? 'http://burfieldcreative.co.uk' : '#'; 
+                        $the_credit     = 'Designed &amp; Developed';
+                        $the_title      = 'Web Design Bath';
+                        $logo_class     = 'site-credit site-credit--dark'
+                    ?>
+                    <?php echo esc_html($the_credit); ?> by <a <?php echo (!is_front_page()) ? 'rel="nofollow"' : false ; ?> class='<?php echo esc_attr($logo_class); ?>' target='_blank' href='<?php echo esc_url($site_address); ?>' title='<?php echo esc_attr($the_title); ?>'><?php echo esc_html('Burfield'); ?><span class='vh'> - <?php echo esc_html($the_title); ?></span></a>
+                </span>
+            </div>
+    
+        </div>
     </div>
-    <nav class="nav-footer col-12 clear">
-      <?php
-      if (has_nav_menu('secondary_navigation')) :
-        wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'footer-menu']);
-      endif;
-      ?>
-    </nav>
-  </div>
-  <div class="island-half sub-footer">
-    <div class="col-12 col-6-tablet">
-      <span class="site-colophon site-colophon--copyright">
-        Copyright © 2016 Original Base theme
-      </span>
-    </div>
-    <div class="col-12 col-6-tablet text-right">
-      <span class="site-scredit-wrap">
-        Designed &amp; Developed by <a rel="nofollow" class="site-credit site-credit--dark" target="_blank" href="#" title="Web Design Bath">Burfield<span class="vh"> - Web Design Bath</span></a>
-      </span>
-    </div>
+
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,40 @@
 <header class="banner">
-  <div class="container">
-    <a class="site-logo" href="<?php echo home_url( '/' ); ?>" title="<?php echo bloginfo('name'); ?>" rel="home">
-	     <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo.png" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-    </a>
-    <nav class="nav-header none block-desktop">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'header-menu']);
-      endif;
-      ?>
-    </nav>
-    <div id="mobile-menu-button" class="hamburger none-desktop block-tablet"><span></span></div>
+    <div class="container row">
+        
+        <div class="col-6 col-4-desktop">
+            <a class="site-logo" href="<?php echo home_url( '/' ); ?>" title="<?php echo bloginfo('name'); ?>" rel="home">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo.png" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+            </a>
+        </div>
+        
+        
+        <div class="col-6 col-8-desktop none block-desktop">
+            
+            <nav class="nav-header">
+                <?php
+                if ( has_nav_menu( 'primary_navigation' ) ) :
+                
+                    $args = array( 
+                        'menu_class' => 'menu header-menu list-inline right',        
+                        'theme_location' => 'primary_navigation',
+                                
+                        'container' => false,
+                        'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                        'depth' => 1,
+                        'fallback_cb' => false,
+                	);
+                	
+                	wp_nav_menu( $args );
+                	
+                endif;
+                ?>
+            </nav>
+        </div>
+        <div class="col-6 none-desktop block-tablet">
+            <div id="mobile-menu-button" class="hamburger"><span></span></div>
+        </div>
+        
+    </div>
+    
   </div>
 </header>
