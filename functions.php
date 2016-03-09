e540e3f213c1b95b031739bf051b646249a6c5b7
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-09 13:04:46 +0000

    inc query controller in functions

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ $sage_includes = [
   'lib/bc/bc-extend-gravity-forms.php',     // Extensions to Gravity Forms
   'lib/bc/bc-is.php',                       // Custom bool functions for state checking -> bc_is_blog_page()
   'lib/bc/bc-protected-name.php',           // Protected name function
+  'lib/bc/bc-query-controller.php',         // Control all queries
   'lib/bc/bc-wp-core.php',                  // Custom tweaks on wordpress
   
   /**
