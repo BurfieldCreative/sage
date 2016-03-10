5766f8e50a6c1b9e46f64acb85796f1d7d31cd25
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-10 09:57:10 +0000

    correct cusoimt page content retreival

--- templates/custom-pages/contact.php
+++ templates/custom-pages/contact.php
@@ -1,3 +1,4 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-<?php get_template_part( 'templates/help'); ?>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
