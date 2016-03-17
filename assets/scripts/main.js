bb42d3987e1af227b5c7a0441ddd655b23ea6651
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-17 14:49:51 +0000

    clean lint warns for match height

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -126,77 +126,77 @@
 
 
 
-    //	Equal height - by Burfield www.burfield.co.uk
-// Example usage use data-match-height="groupName"> on anything!!!
-
-var matchHeight = function () {
-
-    function init() {
-        eventListeners();
-        matchHeight();
-    }
-
-    function eventListeners(){
-        $(window).on('resize', function() {
-            matchHeight();
-        });
-    }
-
-    function matchHeight(){
-        //get all matched height attr
-        var groupName = $('[data-match-height]');
-        var groupHeights = [];
-
-        // for each attr set the min height to auto this makes it responsive
-        $(groupName).each(function(){
-
-            var dataName = $(this);
-
-            var key = dataName.data('match-height');
-
-            //create an array of heights
-            if(!(key in groupHeights)){
-                groupHeights[key] = [];
-            }
-
-            dataName.css('min-height', 'auto');
-
-            dataName.each(function() {
-
-                groupHeights[key].push(dataName.outerHeight());
-
+    // Equal height - by Burfield www.burfield.co.uk
+    // Example usage use data-match-height="groupName"> on anything!!!
+    
+    var matchHeight = (function () {
+        
+        "use strict";
+        
+        function matchHeight(){
+            //get all matched height attr
+            var groupName = $('[data-match-height]');
+            var groupHeights = [];
+    
+            // for each attr set the min height to auto this makes it responsive
+            $(groupName).each(function(){
+    
+                var dataName = $(this);
+    
+                var key = dataName.data('match-height');
+    
+                //create an array of heights
+                if(!(key in groupHeights)){
+                    groupHeights[key] = [];
+                }
+    
+                dataName.css('min-height', 'auto');
+    
+                dataName.each(function() {
+    
+                    groupHeights[key].push(dataName.outerHeight());
+    
+                });
+    
+                return groupHeights.key;
+    
             });
-
-            return groupHeights['key'];
-
-        });
-
-        var obj = groupHeights;
-
-        for (var index in obj) {
-            if (!obj.hasOwnProperty(index)) {
-                continue;
+    
+            var obj = groupHeights;
+    
+            for (var index in obj) {
+                if (!obj.hasOwnProperty(index)) {
+                    continue;
+                }
+    
+                var minHeight = Math.max.apply(null, obj[index]);
+                //set the new min height
+                $('[data-match-height="'+index+'"]').css('min-height', minHeight);
+    
             }
-
-            var minHeight = Math.max.apply(null, obj[index]);
-            //set the new min height
-            $('[data-match-height="'+index+'"]').css('min-height', minHeight);
-
+    
         }
-
-    };
-
-    return {
-        init: init
-    };
-
-} (); //end of equal heights function
-
+        
+        function eventListeners(){
+            $(window).on('resize', function() {
+                matchHeight();
+            });
+        }
+        
+        function init() {
+            eventListeners();
+            matchHeight();
+        }
+        
+        return {
+            init: init
+        };
+    
+    }());
+    
     $(document).ready(function() {
         matchHeight.init();
     });
-
-
 
 
 })(jQuery); // Fully reference jQuery after this point.
