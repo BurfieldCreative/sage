975d65c1121a9a3ca6844e4538c96ccbdaee0448
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-21 15:36:07 +0000

    page header alwasy show for dev purpose

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Titles; ?>
 
 <?php if(!is_front_page()):?>
-  <div class="page-header <?php echo ( ! is_singular() && ! is_page() && ! is_404() ) ? 'sr' : 'show'; ?>">
+  <div class="page-header">
     <h1><?= Titles\title(); ?></h1>
   </div>
 <?php endif;?>
