4899f05c2fe4e21db4031b440c8f76d2f0276023
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-03-22 12:42:44 +0000

    add help to cusotm home

--- templates/custom-pages/home.php
+++ templates/custom-pages/home.php
@@ -1,5 +1,8 @@
+
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part( 'templates/help'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+
+<?php get_template_part( 'templates/help'); ?>
