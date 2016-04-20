6ffca741c8bded02285c80cab3b4f9900b61676d
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-04-20 13:33:34 +0100

    fix layout of post type archive example

--- templates/archive-content.php
+++ templates/archive-content.php
@@ -1,6 +1,14 @@
+<?php get_template_part('templates/page', 'header'); ?>
+
 <?php setup_postdata( $GLOBALS['post'] =& $archive_post->post ); ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-<?php the_content(); ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+    
+            <?php the_content(); ?>
+    
+        </div>
+    </div>
+</div>
 <?php wp_reset_postdata(); ?>
