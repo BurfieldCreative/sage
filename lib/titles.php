ea6030ca231a50ce98662ffd9b26ff3effc6fe11
Neil Berry <neilberry@Neils-MacBook-Pro.local>
2016-04-22 08:30:58 +0100

    make some better page titles

--- lib/titles.php
+++ lib/titles.php
@@ -6,17 +6,47 @@ namespace Roots\Sage\Titles;
  * Page titles
  */
 function title() {
+    
+    
   if (is_home()) {
+      
     if (get_option('page_for_posts', true)) {
       return get_the_title(get_option('page_for_posts', true));
     } else {
       return __('Latest Posts', 'sage');
     }
+    
   } elseif (is_archive()) {
       
-    $post_object = get_post_type_object(get_post_type());
       
-    return apply_filters('the_title', $post_object->label);
+    if(bc_is_custom_post_type()) 
+    {
+        $post_object = get_post_type_object(get_post_type());
+        return apply_filters('the_title', $post_object->label);            
+    }
+    elseif ( is_category() || is_tag() ) {
+		
+		return single_cat_title( '', false );
+
+	} elseif ( is_author() ) {
+		return get_the_author();
+
+	} elseif ( is_day() ) {
+		return get_the_date();
+		
+	} elseif ( is_month() ) {
+		return get_the_date( 'F Y' );
+		
+	} elseif ( is_year() ) {
+		return get_the_date( 'Y' );
+
+	}
+    else
+    {
+        return get_the_archive_title();
+    }
+    
+    
     
   } elseif (is_search()) {
     return sprintf(__('Search Results for %s', 'sage'), get_search_query());
@@ -25,4 +55,6 @@ function title() {
   } else {
     return get_the_title();
   }
+    
+  
 }
