a8de38fcda614a82c745d66c20545534584a9044
steve <steve@steves.local>
2016-07-15 16:00:47 +0100

    updating js media querie background images

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -215,53 +215,34 @@
 
 
 var detect_mq = {
+  live: true, // Boolean: Trigger on window resize, true or false
+  threshold: 200, // Integer: Threshold time after window resize, in milliseconds
+  callback: function () {
 
-    live: true, // Boolean: Trigger on window resize, true or false
-    threshold: 200, // Integer: Threshold time after window resize, in milliseconds
-    callback: function () {
+    var element = document.body;
 
-        var element = document.body;
+    //remove any of our previous classes
+    element.classList.remove( "mobile", "tablet", "desktop", "wide" );
+    //add class to element
+    element.classList.add(dmq_size);
 
-        //remove any of our previous classes
-        element.classList.remove( "mobile", "tablet", "desktop", "wide" );
-        //add class to element
-        element.classList.add(dmq_size);
-        var images = [].slice.call(document.getElementsByClassName("swap-image"));
+    var images = [].slice.call(document.getElementsByClassName("swap-image")),
+        dsize = ["mobile", "tablet", "desktop", "wide"],
+        bgimages = [].slice.call(document.getElementsByClassName("bg-swap-image"));
 
-        if (dmq_size === "mobile") {
-            images = [].slice.call(document.getElementsByClassName("swap-image"));
+    dsize.forEach(function(size, index, array) {
+      if (dmq_size === size) {
+        images.forEach(function(image, id) {
+          imageUrl = image.getAttribute("data-"+size);
+          image.setAttribute("src", imageUrl);
+        });
 
-            images.forEach(function(image, id) {
-                imageUrl = image.getAttribute("data-mobile");
-                image.setAttribute("src", imageUrl);
-            });
-        }
-        if (dmq_size === "tablet") {
-            images = [].slice.call(document.getElementsByClassName("swap-image"));
-
-            images.forEach(function(image, id) {
-                imageUrl = image.getAttribute("data-tablet");
-                image.setAttribute("src", imageUrl);
-            });
-        }
-        if (dmq_size === "desktop") {
-            images = [].slice.call(document.getElementsByClassName("swap-image"));
-
-            images.forEach(function(image, id) {
-                imageUrl = image.getAttribute("data-desktop");
-                image.setAttribute("src", imageUrl);
-            });
-        }
-        if (dmq_size === "wide") {
-            images = [].slice.call(document.getElementsByClassName("swap-image"));
-
-            images.forEach(function(image, id) {
-                imageUrl = image.getAttribute("data-wide");
-                image.setAttribute("src", imageUrl);
-
-            });
-        }
-
-    }
-
+        bgimages.forEach(function(image, id) {
+          bgimageUrl = image.getAttribute("data-"+size);
+          image.style.background = 'url('+bgimageUrl+') no-repeat center center';
+          image.style.backgroundSize = 'cover';
+        });
+      }
+    });
+  }
 };
